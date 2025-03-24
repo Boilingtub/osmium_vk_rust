afc11be2644f7ced28e70f41cd1e0c3b42232a17
Boilingtub <jhbrink59@gmail.com>
2025-03-24 15:32:33 +0200

    comile_shaders.sh script

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 cd ./resources/shaders/
+if [ -d bin ]; then
+  echo "Found shaders/bin directory"
+else 
+  mkdir bin;
+fi
+
 for file in *.glsl ; do
   name="${file%.*}"
   glslc "$file" -o "./bin/${name}.spv"
